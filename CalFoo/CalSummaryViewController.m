98ba765ef134f70afafde6b08c8b395ba42c7382
Wayne O. Cochran <wayne.cochran@gmail.com>
2013-05-21 07:37:06 -0700

    CalSummaryController update display.

--- CalFoo/CalSummaryViewController.m
+++ CalFoo/CalSummaryViewController.m
@@ -8,6 +8,8 @@
 
 #import "CalSummaryViewController.h"
 #import "CalFooAppDelegate.h"
+#import "FoodItem.h"
+#import "ExerciseItem.h"
 
 @interface CalSummaryViewController ()
 
@@ -25,7 +27,42 @@
 -(void)updateSummary {
     CalFooAppDelegate *appDelegate = [[UIApplication sharedApplication] delegate];
     
-    // XXX 
+    float fatGrams = 0;
+    float carbsGrams = 0;
+    float proteinGrams = 0;
+    float totalCalories = 0;
+    for (FoodItem *item in appDelegate.todaysFood) {
+        fatGrams += item.fatGrams;
+        carbsGrams += item.carbsGrams;
+        proteinGrams += item.proteinGrams;
+        totalCalories += item.calories;
+    }
+    
+    float burnedCalories = 0;
+    for (ExerciseItem *item in appDelegate.todaysExercises) {
+        burnedCalories += item.calories;
+    }
+    
+    self.totalCaloriesCell.detailTextLabel.text = [NSString stringWithFormat:@"%0.0f", totalCalories];
+    self.caloriesBurnedCell.detailTextLabel.text = [NSString stringWithFormat:@"%0.0f", burnedCalories];
+    self.netCaloriesCell.detailTextLabel.text = [NSString stringWithFormat:@"%0.0f", totalCalories - burnedCalories];
+    
+    self.fatCell.textLabel.text = [NSString stringWithFormat:@"Fat (%0.0fg)", fatGrams];
+    self.carbsCell.textLabel.text = [NSString stringWithFormat:@"Carbs (%0.0fg)", carbsGrams];
+    self.proteinCell.textLabel.text = [NSString stringWithFormat:@"Protein (%0.0fg)", proteinGrams];
+    
+    const float fatCalsPerGram = 9;
+    const float carbsCalsPerGram = 4;
+    const float proteinCalsPerGram = 4;
+    const float macroCalories = fatCalsPerGram*fatGrams + carbsCalsPerGram*carbsGrams + proteinCalsPerGram*proteinGrams;
+    
+    const float fatPercent = (macroCalories > 0) ? fatGrams*fatCalsPerGram/macroCalories * 100 : 0;
+    const float carbsPercent = (macroCalories > 0) ? carbsGrams*carbsCalsPerGram/macroCalories * 100 : 0;
+    const float proteinPercent = (macroCalories > 0) ? proteinGrams*proteinCalsPerGram/macroCalories * 100 : 0;
+    
+    self.fatCell.detailTextLabel.text = [NSString stringWithFormat:@"%0.0f%%", fatPercent];
+    self.carbsCell.detailTextLabel.text = [NSString stringWithFormat:@"%0.0f%%", carbsPercent];
+    self.proteinCell.detailTextLabel.text = [NSString stringWithFormat:@"%0.0f%%", proteinPercent];
 }
 
 - (void)didReceiveMemoryWarning
@@ -37,7 +74,12 @@
 #pragma mark -- Table view data source  (STATIC CELLS in IB)
 
 - (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
-    return @"May 20, 2013";
+    CalFooAppDelegate *appDelegate = [[UIApplication sharedApplication] delegate];
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    [dateFormatter setDateStyle:NSDateFormatterMediumStyle];
+    [dateFormatter setTimeStyle:NSDateFormatterNoStyle];
+    NSString *formattedDateString = [dateFormatter stringFromDate:appDelegate.today];
+    return formattedDateString;
 }
 
 #pragma mark - Table view delegate
