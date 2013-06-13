113e0977dd9e393a75b91d1160e30f9769514993
Wayne O. Cochran <wcochran@acm.org>
2013-06-12 17:31:48 -0700

    Added "More" View controller.

---
+++ CalFoo/MoreViewController.h
@@ -0,0 +1,13 @@
+//
+//  MoreViewController.h
+//  CalFoo
+//
+//  Created by Wayne Cochran on 6/12/13.
+//  Copyright (c) 2013 Wayne Cochran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MoreViewController : UITableViewController
+
+@end
