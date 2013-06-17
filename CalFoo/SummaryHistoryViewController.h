13b1d7bd42b6f03a96c3f01ecb009790095c900c
Wayne O. Cochran <wcochran@vancouver.wsu.edu>
2013-06-17 13:02:35 -0700

    Added summary history view controller (in progress).

---
+++ CalFoo/SummaryHistoryViewController.h
@@ -0,0 +1,13 @@
+//
+//  HistoryViewController.h
+//  CalFoo
+//
+//  Created by Wayne Cochran on 6/17/13.
+//  Copyright (c) 2013 Wayne Cochran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SummaryHistoryViewController : UITableViewController
+
+@end
