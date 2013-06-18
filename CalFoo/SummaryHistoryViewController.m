66cd076b41b7312beadd25c53e50afeae4f98ff2
Wayne O. Cochran <wcochran@vancouver.wsu.edu>
2013-06-18 12:18:26 -0700

    working on calendar data for summary history view controller.

--- CalFoo/SummaryHistoryViewController.m
+++ CalFoo/SummaryHistoryViewController.m
@@ -9,11 +9,27 @@
 #import "SummaryHistoryViewController.h"
 #import "CalFooAppDelegate.h"
 
+@interface CellData : NSObject
+@property (nonatomic, assign) NSInteger num;
+@property (nonatomic, retain) NSDate *date;
+@property (nonatomic, assign) float fatPercent;
+@property (nonatomic, assign) float carbsPercent;
+@property (nonatomic, assign) float proteinPercent;
+@property (nonatomic, assign) float calories;
+@property (nonatomic, assign) float weight;
+@property (nonatomic, assign) float bodyFat;
+@end
+
+@implementation CellData
+@end
+
 @interface SummaryHistoryViewController ()
 
 @end
 
-@implementation SummaryHistoryViewController
+@implementation SummaryHistoryViewController {
+    NSMutableArray *cellInfo;
+}
 
 - (id)initWithStyle:(UITableViewStyle)style
 {
@@ -27,6 +43,36 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    CalFooAppDelegate *appDelegate = [[UIApplication sharedApplication] delegate];
+    NSMutableDictionary *firstSummaryEntry = [appDelegate.summaryArchive objectAtIndex:0];
+    NSDate *firstDay = [firstSummaryEntry objectForKey:@"date"];
+    NSMutableDictionary *lastSummaryEntry = [appDelegate.summaryArchive lastObject];
+    NSDate *lastDay = [lastSummaryEntry objectForKey:@"date"];
+    
+    NSCalendar *gregorian = [[NSCalendar alloc] initWithCalendarIdentifier:NSGregorianCalendar];
+    
+    //
+    // From Listing 10  Getting the Sunday in the current week:
+    // http://developer.apple.com/library/ios/#documentation/cocoa/Conceptual/DatesAndTimes/Articles/dtCalendricalCalculations.html
+    //
+    NSDateComponents *firstWeekdayComponents = [gregorian components:NSWeekdayCalendarUnit fromDate:firstDay];
+    NSDateComponents *componentsToSubstract = [[NSDateComponents alloc] init];
+    [componentsToSubstract setDay:0 - [firstWeekdayComponents weekday] + 1]; // sunday = 1
+    NSDate *firstSunday = [gregorian dateByAddingComponents:componentsToSubstract toDate:firstDay options:0];
+    
+    //
+    // Listing 13 Days between two dates, as the number of midnights between
+    // http://developer.apple.com/library/ios/#documentation/cocoa/Conceptual/DatesAndTimes/Articles/dtCalendricalCalculations.html
+    //
+    NSInteger startSunday = [gregorian ordinalityOfUnit:NSDayCalendarUnit inUnit:NSEraCalendarUnit forDate:firstSunday];
+    NSInteger startDay = [gregorian ordinalityOfUnit:NSDayCalendarUnit inUnit:NSEraCalendarUnit forDate:firstDay];
+    NSInteger endDay = [gregorian ordinalityOfUnit:NSDayCalendarUnit inUnit:NSEraCalendarUnit forDate:lastDay];
+    
+    const int numDays = endDay - startSunday + 1;
+    cellInfo = [[NSMutableArray alloc] initWithCapacity:numDays];
+    
+    // XXX
 
     // Uncomment the following line to preserve selection between presentations.
     // self.clearsSelectionOnViewWillAppear = NO;
@@ -45,7 +91,13 @@
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-    return 1;
+//    CalFooAppDelegate *appDelegate = [[UIApplication sharedApplication] delegate];
+//    NSMutableDictionary *lastSummaryEntry = [appDelegate.summaryArchive lastObject];
+//    NSDate *lastDate = [lastSummaryEntry objectForKey:@"date"];
+//    const NSInteger lastDay = [gregorian ordinalityOfUnit:NSDayCalendarUnit inUnit:NSEraCalendarUnit forDate:lastDate];
+//    const NSInteger numweeks = (lastDay - startSunday + 1)/7;
+//    return numweeks;
+    return 1; // XXX
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
